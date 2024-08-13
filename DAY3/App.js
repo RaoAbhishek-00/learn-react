cc46589b501cf65ea5a27e8dc327905b70c01681
RaoAbhishek-00 <abhishekrao917@gmail.com>
2024-08-13 19:56:27 +0530

    DAY 3

--- DAY3/App.js
+++ DAY3/App.js
@@ -1,3 +1,10 @@
-const heading = React.createElement("h1",{id : "heading" ,},"website using react cdn links")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading) // rendering 
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "website using react cdn links"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); // rendering
