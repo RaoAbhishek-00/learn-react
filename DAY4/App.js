78fa8a74f4fea404867f124bd2b4dde9cd93f926
RaoAbhishek-00 <abhishekrao917@gmail.com>
2024-08-14 17:47:16 +0530

    day-4

--- DAY4/App.js
+++ DAY4/App.js
@@ -4,14 +4,23 @@ import ReactDOM from "react-dom/client";
 const jsxHeading = <h1>heading using js format </h1>;
 
 const Heading = () => {
- return <h1>its a functional component without using return keyword</h1>
+  return (
+    <div>
+      {jsxHeading}
+      <h1>its a functional component without using return keyword</h1>
+    </div>
+  );
 };
 
-const FunctionWithoutReturnKeyword = () => <h3>function without return keyword </h3>
+const FunctionWithoutReturnKeyword = () => (
+  <h3>function without return keyword </h3>
+);
 
 const HeadingComponent = () => {
   return (
     <div id="container">
+      {jsxHeading}
+      {Heading()}
       <Heading />
       <FunctionWithoutReturnKeyword />
       <p> its a functional component </p>
