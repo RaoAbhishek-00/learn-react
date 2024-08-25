09fc4df8c43f45cbaad0f44b4dbba96e4c2ae473
RaoAbhishek-00 <abhishekrao917@gmail.com>
2024-08-25 12:51:24 +0530

    day 11

--- Day11/src/components/RestaurantItem.js
+++ Day11/src/components/RestaurantItem.js
@@ -4,13 +4,16 @@ const RestaurantItem = ({data})=>{
     // console.log(data);
     const {name, price, ratings ,imageId,description} = data;
     return (
-        <div className=" border-b-2  ">
-            <h2>{name}</h2> 
+        <div className=" border-b-2 flex ">
             
+            <div>
+            <h2>{name}</h2> 
             <span>{ratings.aggregatedRating.rating}</span>
             <span>{price/100}</span> 
             <p>{description}</p>
-            
+            </div>
+          
+            <div><button className="bg-gray-800 text-white p-2 m-2  ">add</button></div>
         </div>
     )
 }
